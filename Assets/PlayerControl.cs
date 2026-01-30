992e584f8437828d4396f627c00bc80761eb0d79
dragonpapay <patrickdpsv17@gmail.com>
2026-01-30 20:10:47 +0100

    first commit

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float speed = 5f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var keyboard = Keyboard.current;
+        float moveX = 0f;
+        if (keyboard.aKey.isPressed) moveX = -1f;
+        if (keyboard.dKey.isPressed) moveX = 1f;
+        float moveY = 0f;
+        if (keyboard.wKey.isPressed) moveY = 1f;
+        if (keyboard.sKey.isPressed) moveY = -1f;
+
+        Vector3 movement = new Vector3(moveX, moveY, 0).normalized * speed * Time.deltaTime;
+        transform.Translate(movement);
+    }
+}
