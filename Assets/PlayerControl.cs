afc64cf1a22c98e349bd1fef531f2c42e67e7cf8
dragonpapay <patrickdpsv17@gmail.com>
2026-01-30 21:08:26 +0100

    canvis jugador afegit

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -4,16 +4,22 @@ using UnityEngine.InputSystem;
 public class PlayerControl : MonoBehaviour
 {
     public float speed = 5f;
+    public float health = 100f;
+    public float shield = 50f;
+    public float attackcooldown = 1f;
+    public float attackdamage = 10f;
+
+    enum playerstate { idle, running, dead, attacking, parrying}
+    playerstate currentstate = playerstate.idle;    
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    void Start(){
         
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
+    void movementhandler(){
+         //movement input handling
         var keyboard = Keyboard.current;
         float moveX = 0f;
         if (keyboard.aKey.isPressed) moveX = -1f;
@@ -24,5 +30,15 @@ public class PlayerControl : MonoBehaviour
 
         Vector3 movement = new Vector3(moveX, moveY, 0).normalized * speed * Time.deltaTime;
         transform.Translate(movement);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate(){
+
+        movementhandler();
+
+
+
+
     }
 }
