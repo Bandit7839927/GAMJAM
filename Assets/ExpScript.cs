de89d39a0df24e6a8adaa2a3e0f4c00130e50a49
dragonpapay <patrickdpsv17@gmail.com>
2026-01-31 00:42:21 +0100

    small fixes

--- Assets/ExpScript.cs
+++ Assets/ExpScript.cs
@@ -4,13 +4,20 @@ public class ExpScript : Collectible
 {
     public int value = 1; 
 
-    protected override void ApplyEffect(GameObject player)
+    protected override void ApplyEffect(GameObject objectThatTouchedMe)
     {
-        PlayerControl playerControl = player.GetComponent<PlayerControl>();
-        
-        if (playerControl != null){
+        // BUSCAMOS EN EL PADRE: Esto soluciona el problema del hijo colisionador
+        PlayerControl playerControl = objectThatTouchedMe.GetComponentInParent<PlayerControl>();
+
+        if (playerControl != null)
+        {
             playerControl.exp += value;
             Debug.Log("¡EXP añadida! Total en el jugador: " + playerControl.exp);
+        }
+        else 
+        {
+            // Si ves esto en la consola, el script PlayerControl no está en el padre
+            Debug.LogWarning("No se encontró PlayerControl en el padre de: " + objectThatTouchedMe.name);
         }
     }
 }
--- Assets/collect_scrip.cs
+++ Assets/collect_scrip.cs
@@ -7,7 +7,7 @@ public abstract class Collectible : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("PlayerHitbox"))
         {
             ApplyEffect(other.gameObject); 
             Destroy(gameObject);          
